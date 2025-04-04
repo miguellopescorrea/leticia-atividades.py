503cb089005c8e24959944e2cb5146fe0546eb61
miguellopescorrea <miguel.correa23@etepd.com>
2025-04-04 12:23:12 +0000

    conversor.py

---
+++ conversor.py
@@ -0,0 +1,19 @@
+km=float(input(""))
+m=float(input(""))
+L=float(input(""))
+def conversor_km(km):
+    return km/0.6
+def conversor_m(m):
+    return m/100
+def conversor_l(L):
+    return L/100
+
+converter = input("")
+
+if converter == "km":
+    print("seu resultado em milhas é: ", conversor_km(km) )
+
+elif converter == "m":
+    print("seu resultado em cemtimetros ém", conversor_m(m))
+elif converter == "l":
+    print("seu resultado em mililitros é", conversor_l(L))
